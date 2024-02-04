a23d02ca3f25514bc06d65a780642b126b66c574
Percy <percy@railway.app>
2024-02-04 20:21:43 +0000

    Initial Commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const http = require('http');
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World!');
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}/`);
+});
