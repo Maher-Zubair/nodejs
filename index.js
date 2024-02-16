40aeb9d475e72e12d7f1f34f4a1da3cd74c6dc0f
ɪɴɴᴏxᴇɴᴛ ᴢᴜʙɪ <143280418+Maher-Zubair@users.noreply.github.com>
2024-02-16 16:53:27 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
-/*
-* @Creator: sigma
-*/
+/**
+
+* @project_name : SIGMA-MD
+* @Developer : Maher-Zubair
+* @Version : v.1.4.9
+* @license : Apache-2.0
+
+This Project Is Under Apache-2.0 License.
+So, No One Have Permission To Copy This Project,
+Reupload, Reversed Engineering And Any Kind Of Deobfuscation.
+Otherwise, A Legal Github Copyright Action Will Be Taken Against You
+Which Result In Removing The Copied Project And Permanantly Banning Of You Account.
+* @Copyright_owner : Maher-Zubair
+
+**/
 
 
 
